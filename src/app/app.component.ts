b325e79c3d6c4dd96c7d7aa9108f1bee0d99c3e1
kabylakee <vctrcute@gmail.com>
2023-06-27 14:20:00 +0300

    add todos view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface Todo {
+  title: string;
+  completed: boolean;
+  id?: number;
+  userId?: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ang-todos';
+export class AppComponent implements OnInit {
+
+  public todos: Todo[] = [];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public ngOnInit(): void {
+    this.httpClient.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe(todos => this.todos = todos);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { FormsModule } from "@angular/forms";
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { FormsModule } from "@angular/forms";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

0024803283acbce39721cf41ca82552190590ac6
kabylakee <vctrcute@gmail.com>
2023-06-28 14:20:00 +0300

    add and upload todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ export interface Todo {
   title: string;
   completed: boolean;
   id?: number;
-  userId?: number;
 }
 
 @Component({
@@ -16,11 +15,33 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   public todos: Todo[] = [];
+  public todoTitle: string = "";
 
   constructor(private httpClient: HttpClient) {
   }
 
   public ngOnInit(): void {
+    this.uploadTodo();
+  }
+
+  public addTodo(): void {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false,
+    }
+
+    this.httpClient.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        this.todos.unshift(todo);
+        this.todoTitle = "";
+      });
+  }
+
+  public uploadTodo(): void {
     this.httpClient.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
       .subscribe(todos => this.todos = todos);
   }
